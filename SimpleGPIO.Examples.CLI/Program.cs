77e68c0c3252dcf926dc98d6269f79a1326a2c13
Steve Desmond <steve@vtsv.ca>
2018-06-26 21:01:51 -0400

    improve CLI example

--- SimpleGPIO.Examples.CLI/Program.cs
+++ SimpleGPIO.Examples.CLI/Program.cs
@@ -1,14 +1,22 @@
-﻿using SimpleGPIO.Boards;
+﻿using System;
+using System.Threading.Tasks;
+using SimpleGPIO.Boards;
 
 namespace SimpleGPIO.Examples.CLI
 {
     internal static class Program
     {
-        private static void Main()
+        private static async Task Main()
         {
             var pi = new RaspberryPi();
             var redLED = pi.Pin16;
             redLED.TurnOn();
+            
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            
+            // I always pick up my playthings, and so
+            // I will show you another good trick that I know
+            pi.Dispose();
         }
     }
 }
