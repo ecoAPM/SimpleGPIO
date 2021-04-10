c2087ce0568077875c03e0520d1ec19bf90f7fc7
Steve Desmond <steve@vtsv.ca>
2021-04-10 19:13:47 -0400

    Fix deprecated MVC warnings

--- SimpleGPIO.Examples.Web/Startup.cs
+++ SimpleGPIO.Examples.Web/Startup.cs
@@ -8,27 +8,23 @@ namespace SimpleGPIO.Examples.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
             services.AddSingleton<RaspberryPi>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
             app.UseDefaultFiles();
             app.UseStaticFiles();
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(c => c.MapControllers());
         }
     }
 }
