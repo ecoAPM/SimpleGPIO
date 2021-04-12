964e3744efbf54563ee7cdf62ede91a1e47e89b9
Steve Desmond <steve@vtsv.ca>
2021-04-11 21:32:11 -0400

    Replace LinuxPinInterface with SystemPinInterface

--- SimpleGPIO/Boards/PinInterfaceFactory.cs
+++ SimpleGPIO/Boards/PinInterfaceFactory.cs
@@ -1,34 +1,13 @@
-﻿using System;
-using System.Device.Gpio;
-using System.Runtime.InteropServices;
+﻿using System.Device.Gpio;
 using SimpleGPIO.GPIO;
-using SimpleGPIO.OS;
 
 namespace SimpleGPIO.Boards
 {
     public static class PinInterfaceFactory
     {
-        public static IPinInterface NewPinInterface(byte bcmIdentifier) => NewPinInterface(bcmIdentifier, false);
-
-        public static IPinInterface NewPinInterface(byte bcmIdentifier, bool experimental)
-        {
-            if(experimental)
-                return new SystemPinInterface(bcmIdentifier, GpioController);
-            
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                return new LinuxPinInterface(bcmIdentifier, FileSystem);
-
-            throw new NotImplementedException($"{GetOSName()} is not yet supported");
-        }
+        public static IPinInterface NewPinInterface(byte bcmIdentifier)
+            => new SystemPinInterface(bcmIdentifier, GpioController);
 
         private static GpioControllerWrapper GpioController => new GpioControllerWrapper(PinNumberingScheme.Logical);
-
-        private static FileSystem FileSystem
-            => new FileSystem(path => new FileInfoWrapper(path), (fs, path, predicate, action) => new FileWatcher(fs, path, predicate, action));
-
-        private static string GetOSName()
-            => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Windows"
-            : (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? "OSX"
-            : "Unknown OS");
     }
 }
--- SimpleGPIO/OS/IFileInfoWrapper.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleGPIO.OS
-{
-    public interface IFileInfoWrapper
-    {
-        bool Exists { get; }
-        bool IsReadOnly { get; }
-        void Refresh();
-    }
-}
