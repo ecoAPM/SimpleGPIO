b9a6eaa40d5b53c54d4446e96c4e13707c9b8cee
Steve Desmond <steve@vtsv.ca>
2021-11-09 14:37:40 -0500

    Make web example program static

--- SimpleGPIO.Examples.Web/Program.cs
+++ SimpleGPIO.Examples.Web/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace SimpleGPIO.Examples.Web
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
