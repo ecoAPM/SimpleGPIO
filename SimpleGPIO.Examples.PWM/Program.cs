2cdef2531e7a549b8e204d73868cc48a19b4b80d
Steve Desmond <steve@vtsv.ca>
2021-12-22 12:54:13 -0500

    Add PWM example

---
+++ SimpleGPIO.Examples.PWM/Program.cs
@@ -0,0 +1,22 @@
+﻿using SimpleGPIO.Boards;
+
+var pi = new RaspberryPi();
+var led = pi.GPIO17;
+var delay = TimeSpan.FromSeconds(1);
+
+led.Strength = 75;
+await Task.Delay(delay);
+
+led.Strength = 25;
+await Task.Delay(delay);
+
+led.Strength = 100;
+await Task.Delay(delay);
+
+led.Strength = 50;
+await Task.Delay(delay);
+
+led.TurnOff();
+await Task.Delay(delay);
+
+pi.Dispose();
