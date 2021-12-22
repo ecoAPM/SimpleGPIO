7d9c68ab9add394325050c02898817c5be62a931
Steve Desmond <steve@vtsv.ca>
2021-12-22 11:02:02 -0500

    Add IPwmChannel interface and PwmChannelWrapper class

---
+++ SimpleGPIO/Device/IPwmChannel.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleGPIO.Device;
+
+public interface IPwmChannel : IDisposable
+{
+	void Start();
+	void Stop();
+	int Frequency { get; set; }
+	double DutyCycle { get; set; }
+}
---
+++ SimpleGPIO/Device/PwmChannelWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Device.Pwm;
+using System.Device.Pwm.Drivers;
+using SimpleGPIO.GPIO;
+
+namespace SimpleGPIO.Device;
+
+public sealed class PwmChannelWrapper : IPwmChannel
+{
+	private readonly PwmChannel _pwm;
+
+	public PwmChannelWrapper(byte pin, IGpioController controller)
+		=> _pwm = new SoftwarePwmChannel(pin, 120, 0, false, controller.Base, false);
+
+	public int Frequency
+	{
+		get => _pwm.Frequency;
+		set => _pwm.Frequency = value;
+	}
+
+	public double DutyCycle
+	{
+		get => _pwm.DutyCycle;
+		set => _pwm.DutyCycle = value;
+	}
+
+	public void Start() => _pwm.Start();
+	public void Stop() => _pwm.Stop();
+
+	public void Dispose() => _pwm.Dispose();
+}
