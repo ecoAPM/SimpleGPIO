d9c1c86d9f5c769f44ba72da91ae1b788f0dc7ad
Steve Desmond <steve@vtsv.ca>
2021-12-21 20:00:55 -0500

    Move PinInterfaceFactory to GPIO namespace

---
+++ SimpleGPIO/GPIO/PinInterfaceFactory.cs
@@ -0,0 +1,9 @@
+namespace SimpleGPIO.GPIO;
+
+public static class PinInterfaceFactory
+{
+	public static IPinInterface NewPinInterface(byte pin)
+		=> new SystemPinInterface(pin, GPIOController);
+
+	private static readonly IGpioController GPIOController = new GpioControllerWrapper();
+}
