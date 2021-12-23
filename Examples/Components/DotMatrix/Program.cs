c53689c6952ac6df468bff1a3c8b3eece0f7acdf
Steve Desmond <steve@vtsv.ca>
2021-12-23 15:15:09 -0500

    Convert 8-param DotMatrix methods to single set param

--- Examples/Components/DotMatrix/Program.cs
+++ Examples/Components/DotMatrix/Program.cs
@@ -7,25 +7,10 @@ var matrix = new DotMatrix(
 	new DotMatrix.PinSet
 	{
 		//rows
-		Pin1 = pi.GPIO5,
-		Pin2 = pi.GPIO7,
-		Pin3 = pi.GPIO12,
-		Pin4 = pi.GPIO13,
-		Pin5 = pi.GPIO8,
-		Pin6 = pi.GPIO15,
-		Pin7 = pi.GPIO6,
-		Pin8 = pi.GPIO3,
+		Pin1 = pi.GPIO5, Pin2 = pi.GPIO7, Pin3 = pi.GPIO12, Pin4 = pi.GPIO13, Pin5 = pi.GPIO8, Pin6 = pi.GPIO15, Pin7 = pi.GPIO6, Pin8 = pi.GPIO3,
 
 		//columns
-		Pin9 = pi.GPIO1,
-		Pin10 = pi.GPIO14,
-		Pin11 = pi.GPIO16,
-		Pin12 = pi.GPIO4,
-		Pin13 = pi.GPIO11,
-		Pin14 = pi.GPIO2,
-		Pin15 = pi.GPIO17,
-		Pin16 = pi.GPIO18
-	}
+		Pin9 = pi.GPIO1, Pin10 = pi.GPIO14, Pin11 = pi.GPIO16, Pin12 = pi.GPIO4, Pin13 = pi.GPIO11, Pin14 = pi.GPIO2, Pin15 = pi.GPIO17, Pin16 = pi.GPIO18 }
 );
 
 matrix.SetAllRows(PowerValue.Off);
@@ -57,20 +42,20 @@ for (var x = 0; x < 3; x++)
 	matrix.SetAll(PowerValue.Off);
 	await Task.Delay(100);
 
-	matrix.SetRows(PowerValue.Off, PowerValue.Off, PowerValue.Off, PowerValue.On, PowerValue.On, PowerValue.Off, PowerValue.Off, PowerValue.Off);
-	matrix.SetColumns(PowerValue.Off, PowerValue.Off, PowerValue.Off, PowerValue.On, PowerValue.On, PowerValue.Off, PowerValue.Off, PowerValue.Off);
+	matrix.SetRows(new DotMatrix.PowerSet { Item1 = PowerValue.Off, Item2 = PowerValue.Off, Item3 = PowerValue.Off, Item4 = PowerValue.On, Item5 = PowerValue.On, Item6 = PowerValue.Off, Item7 = PowerValue.Off, Item8 = PowerValue.Off });
+	matrix.SetColumns(new DotMatrix.PowerSet { Item1 = PowerValue.Off, Item2 = PowerValue.Off, Item3 = PowerValue.Off, Item4 = PowerValue.On, Item5 = PowerValue.On, Item6 = PowerValue.Off, Item7 = PowerValue.Off, Item8 = PowerValue.Off });
 	await Task.Delay(100);
 
-	matrix.SetRows(PowerValue.Off, PowerValue.Off, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.Off, PowerValue.Off);
-	matrix.SetColumns(PowerValue.Off, PowerValue.Off, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.Off, PowerValue.Off);
+	matrix.SetRows(new DotMatrix.PowerSet { Item1 = PowerValue.Off, Item2 = PowerValue.Off, Item3 = PowerValue.On, Item4 = PowerValue.On, Item5 = PowerValue.On, Item6 = PowerValue.On, Item7 = PowerValue.Off, Item8 = PowerValue.Off });
+	matrix.SetColumns(new DotMatrix.PowerSet { Item1 = PowerValue.Off, Item2 = PowerValue.Off, Item3 = PowerValue.On, Item4 = PowerValue.On, Item5 = PowerValue.On, Item6 = PowerValue.On, Item7 = PowerValue.Off, Item8 = PowerValue.Off });
 	await Task.Delay(100);
 
-	matrix.SetRows(PowerValue.Off, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.Off);
-	matrix.SetColumns(PowerValue.Off, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.Off);
+	matrix.SetRows(new DotMatrix.PowerSet { Item1 = PowerValue.Off, Item2 = PowerValue.On, Item3 = PowerValue.On, Item4 = PowerValue.On, Item5 = PowerValue.On, Item6 = PowerValue.On, Item7 = PowerValue.On, Item8 = PowerValue.Off });
+	matrix.SetColumns(new DotMatrix.PowerSet { Item1 = PowerValue.Off, Item2 = PowerValue.On, Item3 = PowerValue.On, Item4 = PowerValue.On, Item5 = PowerValue.On, Item6 = PowerValue.On, Item7 = PowerValue.On, Item8 = PowerValue.Off });
 	await Task.Delay(100);
 
-	matrix.SetRows(PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On);
-	matrix.SetColumns(PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On, PowerValue.On);
+	matrix.SetRows(new DotMatrix.PowerSet { Item1 = PowerValue.On, Item2 = PowerValue.On, Item3 = PowerValue.On, Item4 = PowerValue.On, Item5 = PowerValue.On, Item6 = PowerValue.On, Item7 = PowerValue.On, Item8 = PowerValue.On });
+	matrix.SetColumns(new DotMatrix.PowerSet { Item1 = PowerValue.On, Item2 = PowerValue.On, Item3 = PowerValue.On, Item4 = PowerValue.On, Item5 = PowerValue.On, Item6 = PowerValue.On, Item7 = PowerValue.On, Item8 = PowerValue.On });
 	await Task.Delay(100);
 }
 
--- SimpleGPIO/Components/DotMatrix.cs
+++ SimpleGPIO/Components/DotMatrix.cs
@@ -12,39 +12,62 @@ public sealed class DotMatrix
 	{
 		Row = new[] { input.Pin9, input.Pin14, input.Pin8, input.Pin12, input.Pin1, input.Pin7, input.Pin2, input.Pin5 };
 		Column = new[] { input.Pin13, input.Pin3, input.Pin4, input.Pin10, input.Pin6, input.Pin11, input.Pin15, input.Pin16 };
+
 		foreach (var column in Column)
+		{
 			column.PowerMode = PowerMode.Differential;
+		}
 	}
 
-	public void SetRows(PowerValue row1, PowerValue row2, PowerValue row3, PowerValue row4, PowerValue row5, PowerValue row6, PowerValue row7, PowerValue row8)
+	public void SetRows(PowerSet rows)
 	{
-		Row[0].Power = row1;
-		Row[1].Power = row2;
-		Row[2].Power = row3;
-		Row[3].Power = row4;
-		Row[4].Power = row5;
-		Row[5].Power = row6;
-		Row[6].Power = row7;
-		Row[7].Power = row8;
+		Row[0].Power = rows.Item1;
+		Row[1].Power = rows.Item2;
+		Row[2].Power = rows.Item3;
+		Row[3].Power = rows.Item4;
+		Row[4].Power = rows.Item5;
+		Row[5].Power = rows.Item6;
+		Row[6].Power = rows.Item7;
+		Row[7].Power = rows.Item8;
 	}
 
-	public void SetColumns(PowerValue col1, PowerValue col2, PowerValue col3, PowerValue col4, PowerValue col5, PowerValue col6, PowerValue col7, PowerValue col8)
+	public void SetColumns(PowerSet columns)
 	{
-		Column[0].Power = col1;
-		Column[1].Power = col2;
-		Column[2].Power = col3;
-		Column[3].Power = col4;
-		Column[4].Power = col5;
-		Column[5].Power = col6;
-		Column[6].Power = col7;
-		Column[7].Power = col8;
+		Column[0].Power = columns.Item1;
+		Column[1].Power = columns.Item2;
+		Column[2].Power = columns.Item3;
+		Column[3].Power = columns.Item4;
+		Column[4].Power = columns.Item5;
+		Column[5].Power = columns.Item6;
+		Column[6].Power = columns.Item7;
+		Column[7].Power = columns.Item8;
 	}
 
 	public void SetAllRows(PowerValue power)
-		=> SetRows(power, power, power, power, power, power, power, power);
+		=> SetRows(new PowerSet
+		{
+			Item1 = power,
+			Item2 = power,
+			Item3 = power,
+			Item4 = power,
+			Item5 = power,
+			Item6 = power,
+			Item7 = power,
+			Item8 = power
+		});
 
 	public void SetAllColumns(PowerValue power)
-		=> SetColumns(power, power, power, power, power, power, power, power);
+		=> SetColumns(new PowerSet
+		{
+			Item1 = power,
+			Item2 = power,
+			Item3 = power,
+			Item4 = power,
+			Item5 = power,
+			Item6 = power,
+			Item7 = power,
+			Item8 = power
+		});
 
 	public void SetAll(PowerValue power)
 	{
@@ -70,5 +93,17 @@ public sealed class DotMatrix
 		public IPinInterface Pin14 { get; init; } = null!;
 		public IPinInterface Pin15 { get; init; } = null!;
 		public IPinInterface Pin16 { get; init; } = null!;
+	}
+
+	public sealed class PowerSet
+	{
+		public PowerValue Item1 { get; init; }
+		public PowerValue Item2 { get; init; }
+		public PowerValue Item3 { get; init; }
+		public PowerValue Item4 { get; init; }
+		public PowerValue Item5 { get; init; }
+		public PowerValue Item6 { get; init; }
+		public PowerValue Item7 { get; init; }
+		public PowerValue Item8 { get; init; }
 	}
 }
