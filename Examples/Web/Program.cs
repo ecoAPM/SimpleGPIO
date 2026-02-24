557fe72036fad56a18240be8747cc1007e051dd1
Steve Desmond <Steve@ecoAPM.com>
2026-02-24 11:54:48 -0500

    Replace startup class in web example with standalone methods

--- Examples/Web/Program.cs
+++ Examples/Web/Program.cs
@@ -1,12 +1,31 @@
-using Microsoft.AspNetCore;
+using SimpleGPIO.Boards;
 
 namespace SimpleGPIO.Examples.Web;
 
 public static class Program
 {
-	public static void Main(string[] args)
+	public static async Task Main(string[] args)
 	{
-		WebHost.CreateDefaultBuilder(args)
-			.UseStartup<Startup>().Build().Run();
+		var builder = WebApplication.CreateBuilder(args);
+		ConfigureServices(builder.Services);
+
+		var app = builder.Build();
+		Configure(app);
+
+		await app.RunAsync();
+	}
+
+	private static void ConfigureServices(IServiceCollection services)
+	{
+		services.AddMvc();
+		services.AddSingleton<RaspberryPi>();
+	}
+
+	private static void Configure(IApplicationBuilder app)
+	{
+		app.UseDefaultFiles();
+		app.UseStaticFiles();
+		app.UseRouting();
+		app.UseEndpoints(c => c.MapControllers());
 	}
 }
